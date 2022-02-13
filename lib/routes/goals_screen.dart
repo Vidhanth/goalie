6acc72b3a2f755b7861d60849779bea98d8ac319
Vidhanth Bafna <vidhanthbafna@gmail.com>
2022-02-13 09:31:21 +0530

    Remove flutter boilerplate code and create initial screens

---
+++ lib/routes/goals_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GoalsScreen extends StatelessWidget {
+  const GoalsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Goals'),
+    );
+  }
+}
