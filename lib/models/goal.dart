3267f563a71946a9b8463fd80240f47c639301ef
Vidhanth Bafna <vidhanthbafna@gmail.com>
2022-03-01 00:46:45 +0530

    Changes to Goal model class * Assign current date when object is created * Add copyWith function

--- lib/models/goal.dart
+++ lib/models/goal.dart
@@ -1,15 +1,28 @@
+import 'package:goalie/utils/date_helper.dart';
+
 class Goal {
   Goal({
     this.id,
     required this.text,
     required this.color,
     this.createdAt,
-  });
+  }) {
+    createdAt ??= DateHelper.getDatabaseDate(DateTime.now());
+  }
 
   int? id;
   String text;
   int color;
   String? createdAt;
+
+  Goal copyWith({int? id, String? text, int? color, String? createdAt}) {
+    return Goal(
+      id: id ?? this.id,
+      text: text ?? this.text,
+      color: color ?? this.color,
+      createdAt: createdAt ?? this.createdAt,
+    );
+  }
 
   factory Goal.fromMap(Map<String, dynamic> json) => Goal(
         id: json["id"],
