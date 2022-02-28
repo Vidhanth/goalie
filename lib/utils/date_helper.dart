26a9649cc92c545f00a6c9b8be09b83f643985e9
Vidhanth Bafna <vidhanthbafna@gmail.com>
2022-03-01 00:46:10 +0530

    Add DateHelper Utility class

---
+++ lib/utils/date_helper.dart
@@ -0,0 +1,37 @@
+import 'package:intl/intl.dart';
+
+class DateHelper {
+  static String getDatabaseDate(DateTime date) {
+    DateFormat formatter = DateFormat('yyyy-MM-dd');
+    return formatter.format(date);
+  }
+
+  static String getDisplayDate(String dateString) {
+    DateTime date = DateTime.parse(dateString);
+    DateFormat formatter = DateFormat(DateFormat.ABBR_MONTH_DAY);
+
+    if (date.isToday) {
+      return 'Today';
+    }
+
+    if (date.isYesterday) {
+      return 'Yesterday';
+    }
+
+    return formatter.format(date);
+  }
+}
+
+extension DateExtensions on DateTime {
+  bool get isToday {
+    final now = DateTime.now();
+    return now.day == day && now.month == month && now.year == year;
+  }
+
+  bool get isYesterday {
+    final yesterday = DateTime.now().subtract(const Duration(days: 1));
+    return yesterday.day == day &&
+        yesterday.month == month &&
+        yesterday.year == year;
+  }
+}
